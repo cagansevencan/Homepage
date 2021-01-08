c469cf6fd367698495db1903c4f23874bba3df48
Cagan Sevencan <cagansvncn@gmail.com>
2021-01-08 13:36:54 -0800

    Chakra Setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import theme from '../theme'
+import { ChakraProvider, CSSReset } from "@chakra-ui/react"
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <ChakraProvider theme={theme}>
+            <CSSReset/>
+            <Component {...pageProps} />
+        </ChakraProvider>
+      </>
+  )
 }
 
 export default MyApp
