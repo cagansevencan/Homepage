e55ab4a51b7486cb9fa3d099e09867c8f48d7fee
Cagan Sevencan <cagansvncn@gmail.com>
2021-01-09 16:14:42 -0800

    Config file

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const withPWA = require('next-pwa')
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+const nextConfig = (phase) => {
+    const isDev = phase === PHASE_DEVELOPMENT_SERVER
+
+    return {
+        env: {
+            API_URL: isDev ? 'http://localhost:3000' : 'https://cagansevencan.com'
+        },
+        images: {
+            domains: ['dl.airtable.com']
+        },
+        pwa: {
+            dest: 'public',
+            disable: isDev
+        },
+        experimental: {
+            optimizeFonts: true,
+            modern: true
+        }
+    }
+}
+
+module.exports = (phase) => withPWA(nextConfig(phase))
