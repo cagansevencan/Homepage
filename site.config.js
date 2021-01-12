9d525f02d3e94a5b3ce70f61de5437be9a013835
Cagan Sevencan <cagansvncn@gmail.com>
2021-01-11 16:35:46 -0800

    Socials

---
+++ site.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Next Template',
+  description: 'Next PWA Template',
+  lang: 'en',
+  siteUrl: 'https://cagansevencan.com',
+  author: {
+    email: 'cagansvncn@gmail.com',
+    name: 'Cagan Sevencan'
+  },
+  social: {
+    instagram: 'https://instagram.com/cgnsvncn',
+    twitter: 'https://twitter.com/cagan_dev',
+    medium: 'https://medium.com/@cagan.sevencan',
+    github: 'https://github.com/cagansevencan',
+    youtube: 'https://www.youtube.com/cagan8',
+  }
+}
